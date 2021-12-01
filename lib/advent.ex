e358a0668de2cbc4eb95053a432ea69801f0c759
Andrew Stewart <andrew@stwrt.ca>
2021-11-30 21:13:20 -0800

    more setup + day 01

--- lib/advent.ex
+++ lib/advent.ex
@@ -1,18 +1,10 @@
 defmodule Advent do
-  @moduledoc """
-  Documentation for `Advent`.
-  """
+  @moduledoc "Helpers for Advent of Code puzzles."
 
-  @doc """
-  Hello world.
+  ## Loading Data
 
-  ## Examples
-
-      iex> Advent.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  def stream_data!("" <> file), do: [data_dir(), file] |> Path.join() |> File.stream!()
+  def read_data!("" <> file), do: file |> stream_data!() |> Enum.to_list()
+  def data_dir, do: [priv_dir(), "data"] |> Path.join()
+  def priv_dir, do: :code.priv_dir(:advent)
 end
---
+++ lib/advent/day01.exs
@@ -0,0 +1,17 @@
+data = Advent.stream_data!("day01")
+
+depths =
+  for line <- data,
+  do: line |> String.trim() |> String.to_integer()
+
+depths
+|> Enum.chunk_every(2, 1, :discard)
+|> Enum.count(fn [one, two] -> two > one end)
+|> IO.inspect(label: "part 1")
+
+depths
+|> Stream.chunk_every(3, 1, :discard)
+|> Stream.map(fn [one, two, three] -> one + two + three end)
+|> Stream.chunk_every(2, 1, :discard)
+|> Enum.count(fn [one, two] -> two > one end)
+|> IO.inspect(label: "part 2")
