d2aba27f97940dca790401ffe54081437911a87e
Andrew Stewart <andrew@stwrt.ca>
2021-12-04 18:03:47 -0800

    day 04 answers

--- lib/advent/day04.exs
+++ lib/advent/day04.exs
@@ -1,61 +1,45 @@
 defmodule Board do
-  # boards are a 5/5 grid
-  @size 5
-
   # each space can be :marked or :unmarked
   defstruct [:spaces]
 
   def new([r | _] = rows) when is_list(r) do
-    spaces = Enum.map(rows, fn row ->
-      Enum.map(row, & {&1, :unmarked})
-    end)
+    spaces =
+      Enum.map(rows, fn row ->
+        Enum.map(row, &{&1, :unmarked})
+      end)
 
     %__MODULE__{
       spaces: spaces
     }
   end
 
-  def call(%__MODULE__{spaces: spaces} = board, number) do
-    all_unmarked_spaces_matching_number =
-      [
-        Access.all(),
-        Access.filter(fn {num, status} -> num == number && status == :unmarked end)
-      ]
+  def mark(%__MODULE__{spaces: spaces} = board, number) do
+    all_unmarked_spaces_matching_number = [
+      Access.all(),
+      Access.filter(fn {num, status} -> num == number && status == :unmarked end)
+    ]
 
-    new_spaces =
-      update_in(
-        spaces,
-        all_unmarked_spaces_matching_number,
-        fn {^number, :unmarked} -> {number, :marked} end
-      )
-
-    %__MODULE__{board | spaces: new_spaces}
+    mark = fn {^number, :unmarked} -> {number, :marked} end
+    %__MODULE__{board | spaces: update_in(spaces, all_unmarked_spaces_matching_number, mark)}
   end
 
-  def won?(%__MODULE__{} = board) do
+  def bingo?(%__MODULE__{} = board) do
     winning_columns?(board) || winning_rows?(board)
   end
 
-  def winning_rows?(%__MODULE__{spaces: spaces}) do
-    Enum.any?(spaces, fn row ->
-      Enum.all?(row, fn {_, status} -> status == :marked end)
-    end)
+  def winning_rows?(%__MODULE__{spaces: rows}) do
+    all_marked? = &Enum.all?(&1, fn {_, status} -> status == :marked end)
+    Enum.any?(rows, all_marked?)
   end
 
   def winning_columns?(%__MODULE__{spaces: spaces}) do
-    columns = Enum.zip(spaces)
-
-    Enum.any?(columns, fn col ->
-      Enum.all?(row, fn {_, status} -> status == :marked end)
-    end)
+    columns = spaces |> Stream.zip() |> Stream.map(&Tuple.to_list/1)
+    all_marked? = &Enum.all?(&1, fn {_, status} -> status == :marked end)
+    Enum.any?(columns, all_marked?)
   end
 
   def score(%__MODULE__{spaces: spaces}, final_called_number) do
-    sum_of_unmarked_spaces =
-      spaces
-      |> Enum.flat_map(fn row -> for {n, :unmarked} <- row, do: n end)
-      |> Enum.sum()
-
+    sum_of_unmarked_spaces = Enum.sum(for row <- spaces, {n, :unmarked} <- row, do: n)
     sum_of_unmarked_spaces * final_called_number
   end
 end
@@ -64,13 +48,11 @@ defmodule Bingo do
   def parse("" <> input) do
     import String, only: [split: 2, split: 3, to_integer: 1]
 
-    parse_line =
-      fn line -> line |> split(" ", trim: true) |> Enum.map(&to_integer/1) end
+    parse_line = fn line -> line |> split(" ", trim: true) |> Enum.map(&to_integer/1) end
 
     [numbers_str | boards_strs] = split(input, "\n", trim: true)
 
-    numbers =
-      for n <- split(numbers_str, ","), do: to_integer(n)
+    numbers = for n <- split(numbers_str, ","), do: to_integer(n)
 
     boards =
       boards_strs
@@ -85,6 +67,30 @@ end
 input = Advent.read_data!("day04")
 {numbers, boards} = Bingo.parse(input)
 
-winning_board =
-  Enum.reduce_while(Stream.cycle([:ok]), {boards, numbers}, fn _, {boards, [number | next]} ->
-  end)
+# find the first board that will win
+Enum.reduce_while(numbers, {boards, _called = []}, fn
+  number, {boards, called} ->
+    updated_boards =
+      for board <- boards,
+          do: Board.mark(board, number)
+
+    case Enum.split_with(updated_boards, &Board.bingo?/1) do
+      {[winner | _], _} -> {:halt, Board.score(winner, number)}
+      {_, in_play} -> {:cont, {in_play, [number | called]}}
+    end
+end)
+|> IO.inspect(label: "Part 1")
+
+# find the _last_ board that will win
+Enum.reduce_while(numbers, {boards, _called = []}, fn
+  number, {boards, called} ->
+    updated_boards =
+      for board <- boards,
+          do: Board.mark(board, number)
+
+    case Enum.split_with(updated_boards, &Board.bingo?/1) do
+      {[winner | _], []} -> {:halt, Board.score(winner, number)}
+      {_, in_play} -> {:cont, {in_play, [number | called]}}
+    end
+end)
+|> IO.inspect(label: "Part 2")
